3653eee07a065ed00dda5762399d9aee3b12b7bc
Rohit DS <Rohit_DS@outlook.com>
2025-03-21 22:01:51 +0530

    working drop-down menu for 911 Series

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ bars.addEventListener("click", () => {
 closingButton.addEventListener("click", () => {
   sidebar.classList.remove("show-sidebar");
 });
+
+const Nine11 = document.querySelector(".NineEleven")
+const Var911 = document.querySelector(".sub-menu-911")
+
+Nine11.addEventListener("click" , () => {
+  Var911.classList.toggle("open-menu");
+});
