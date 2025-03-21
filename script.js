73048ddc40325633e822d38c8f7cdd7e861c1915
Rohit DS <Rohit_DS@outlook.com>
2025-03-21 22:18:02 +0530

    Added Car Varients Under Porsche 718 Sereis

--- script.js
+++ script.js
@@ -16,3 +16,10 @@ const Var911 = document.querySelector(".sub-menu-911")
 Nine11.addEventListener("click" , () => {
   Var911.classList.toggle("open-menu");
 });
+
+const Seven18 = document.querySelector(".SevenOneEight")
+const Var718 = document.querySelector(".sub-menu-718")
+
+Seven18.addEventListener("click" , () => {
+  Var718.classList.toggle("open-menu");
+});
